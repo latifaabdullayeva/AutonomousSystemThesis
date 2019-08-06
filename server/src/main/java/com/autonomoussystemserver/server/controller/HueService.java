8f5ad442841cef1562f479f28412f2edbe61bc87
Latifa <latifaabdullayeva@gmail.com>
2019-08-06 18:39:01 +0200

    Created class HueService

--- server/src/main/java/com/autonomoussystemserver/server/controller/HueService.java
+++ server/src/main/java/com/autonomoussystemserver/server/controller/HueService.java
@@ -1,4 +1,19 @@
 package com.autonomoussystemserver.server.controller;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public enum HueService {
+    ;
+
+    // in order to change the behavior of lamps, we need to do Put request
+    @PUT("{username}/lights/{lightNumber}/state/")
+    public Call<ResponseBody> updateHueLamp(@Path("username") String username,
+                                            @Path("lightNumber") int light,
+                                            @Body HueRequest hueRequest) {
+        return null;
+    }
 }
