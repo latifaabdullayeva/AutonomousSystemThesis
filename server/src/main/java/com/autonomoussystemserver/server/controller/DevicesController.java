9dc2c6cca5387ce7025350e3b5661aa31413726f
Latifa <latifaabdullayeva@gmail.com>
2019-08-06 18:38:04 +0200

    Created class HueRequest

--- server/src/main/java/com/autonomoussystemserver/server/controller/DevicesController.java
+++ server/src/main/java/com/autonomoussystemserver/server/controller/DevicesController.java
@@ -8,10 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import sun.rmi.runtime.Log;
+
+import java.io.IOException;
 
 // GET --> POST
 @RestController
@@ -45,9 +54,50 @@ public class DevicesController {
             devicesRepository.save(newDevice);
             return ResponseEntity.ok(newDevice);
         }
-
         // TODO: problem to solve
         // query all hue lamps from database
         // send request to all hue lamps to turn light on
+    }
+
+    private final HueService hueService;
+    private final String username;
+
+    public DevicesController(String ipAddress, String user) {
+//         The Retrofit class generates an implementation of the HueService interface.
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + ipAddress + "/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+//         By default, Retrofit can only deserialize HTTP bodies into OkHttp's ResponseBody type
+//         and it can only accept its RequestBody type for @Body.
+//         Converters can be added to support other types. Gson: com.squareup.retrofit2:converter-gson
+//         GsonConverterFactory class is needed to generate an implementation of the HueService interface
+//         which uses Gson for its deserialization.
+        hueService = retrofit.create(HueService.class);
+        username = user;
+    }
+
+    public void updateBrightness(int brightness) {
+        HueRequest request = new HueRequest(true, brightness);
+
+        hueService.updateHueLamp(username, 1, request)
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                        System.out.println("HueRepository" + " success!");
+//                        try {
+//                            if (response.body() != null) {
+//                                System.out.println("HueRepository" + response.body());
+//                            }
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                        System.out.println("HueRepository" + " failure : " + t);
+                    }
+                });
     }
 }
---
+++ server/src/main/java/com/autonomoussystemserver/server/controller/HueRequest.java
@@ -0,0 +1,19 @@
+package com.autonomoussystemserver.server.controller;
+
+public class HueRequest {
+    private final boolean on;
+    private final int bri;
+
+    HueRequest(boolean on, int bri) {
+        this.on = on;
+        this.bri = bri;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public int getBri() {
+        return bri;
+    }
+}
