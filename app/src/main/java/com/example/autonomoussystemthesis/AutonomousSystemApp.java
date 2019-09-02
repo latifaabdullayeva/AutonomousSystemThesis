aacb572506f65525be3efa204afd5529c35d9d27
Latifa <latifaabdullayeva@gmail.com>
2019-09-02 18:53:39 +0200

    Remove all Beacon Manager logs

--- app/src/main/java/com/example/autonomoussystemthesis/AutonomousSystemApp.java
+++ app/src/main/java/com/example/autonomoussystemthesis/AutonomousSystemApp.java
@@ -1,6 +1,7 @@
 package com.example.autonomoussystemthesis;
 
 import android.app.Application;
+import android.util.Log;
 
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
@@ -10,7 +11,7 @@ public class AutonomousSystemApp extends Application {
 
     public void onCreate() {
         super.onCreate();
-
+        Log.d("FLOW", "AutonomousSystemApp");
         setupBeaconManager();
     }
 
@@ -18,9 +19,10 @@ public class AutonomousSystemApp extends Application {
         BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
 
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
-        BeaconManager.setDebug(true);
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        );
+        // BeaconManager.setDebug(true);
 
         // enables auto battery saving of about 60%
         new BackgroundPowerSaver(this);
