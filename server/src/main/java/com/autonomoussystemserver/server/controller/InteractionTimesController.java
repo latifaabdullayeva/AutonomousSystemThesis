328b4df88bbf1f6d1b6cfb0a21a73b188b987553
Latifa <latifaabdullayeva@gmail.com>
2019-09-03 19:02:45 +0200

    Adds POST request for number of interactions (has internal server error)

--- server/src/main/java/com/autonomoussystemserver/server/controller/InteractionTimesController.java
+++ server/src/main/java/com/autonomoussystemserver/server/controller/InteractionTimesController.java
@@ -1,5 +1,7 @@
 package com.autonomoussystemserver.server.controller;
 
+import com.autonomoussystemserver.server.controller.model.InteractionTimesDto;
+import com.autonomoussystemserver.server.database.model.Devices;
 import com.autonomoussystemserver.server.database.model.InteractionTimes;
 import com.autonomoussystemserver.server.database.repository.InteractionTimesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,6 +25,31 @@ public class InteractionTimesController {
         System.out.println("------------------------------------------------------------");
         System.out.println("InteractionTimesController -> GET getInteractionTimes()");
         return interactionTimesRepository.findAll(pageable);
+    }
+
+    @PostMapping("/interactionTimes")
+    public ResponseEntity<InteractionTimes> createInteraction(@RequestBody InteractionTimesDto interactionTimesDto) {
+        System.out.println("------------------------------------------------------------");
+        System.out.println("InteractionTimesController -> POST createInteraction()");
+
+        InteractionTimes existingInteraction = interactionTimesRepository.findByMascotId(interactionTimesDto.getMascotId().getDeviceId());
+
+        if (existingInteraction != null) {
+            return ResponseEntity.badRequest().body(null);
+        } else {
+            InteractionTimes newInteractionTimes = new InteractionTimes();
+//            if (interactionTimesDto.getMascotId().equals("Mascot")) {}
+            if (interactionTimesDto.getMascotId() != null) {
+                Devices devices = new Devices();
+                devices.setDeviceId(interactionTimesDto.getMascotId().getDeviceId());
+
+                newInteractionTimes.setMascotId(devices);
+            }
+            newInteractionTimes.setInteractionTimes(interactionTimesDto.getInteractionTimes());
+
+            interactionTimesRepository.save(newInteractionTimes);
+            return ResponseEntity.ok(newInteractionTimes);
+        }
     }
 
 }
