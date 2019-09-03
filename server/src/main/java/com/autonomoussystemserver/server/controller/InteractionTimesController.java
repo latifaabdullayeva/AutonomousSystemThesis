39db98a0fb65f0b6adea18971c00f01bc57bf037
Latifa <latifaabdullayeva@gmail.com>
2019-09-03 17:14:02 +0200

    Adds GET request for number of interactions and Interaction Repository

--- server/src/main/java/com/autonomoussystemserver/server/controller/InteractionTimesController.java
+++ server/src/main/java/com/autonomoussystemserver/server/controller/InteractionTimesController.java
@@ -1,4 +1,27 @@
 package com.autonomoussystemserver.server.controller;
 
+import com.autonomoussystemserver.server.database.model.InteractionTimes;
+import com.autonomoussystemserver.server.database.repository.InteractionTimesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
 public class InteractionTimesController {
+
+    @Autowired
+    private InteractionTimesRepository interactionTimesRepository;
+
+    @GetMapping("/interactionTimes")
+    public Page<InteractionTimes> getInteractionTimes(Pageable pageable) {
+        System.out.println("------------------------------------------------------------");
+        System.out.println("InteractionTimesController -> GET getInteractionTimes()");
+        return interactionTimesRepository.findAll(pageable);
+    }
+
 }
--- server/src/main/java/com/autonomoussystemserver/server/database/repository/InteractionTimesRepository.java
+++ server/src/main/java/com/autonomoussystemserver/server/database/repository/InteractionTimesRepository.java
@@ -1,4 +1,12 @@
 package com.autonomoussystemserver.server.database.repository;
 
-public class InteractionTimesRepository {
+import com.autonomoussystemserver.server.database.model.InteractionTimes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InteractionTimesRepository extends JpaRepository<InteractionTimes, Integer>, CrudRepository<InteractionTimes, Integer> {
+
+    @Query("select interaction from InteractionTimes interaction where mascotId=:mascotId")
+    InteractionTimes findByMascotId(Integer mascotId);
 }
