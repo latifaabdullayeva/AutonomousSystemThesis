550d520ef338ca44f482dcb1081882616ff2f86e
Latifa <latifaabdullayeva@gmail.com>
2019-09-03 16:09:25 +0200

    Updates Data object Transfer

--- server/src/main/java/com/autonomoussystemserver/server/controller/model/InteractionTimesDto.java
+++ server/src/main/java/com/autonomoussystemserver/server/controller/model/InteractionTimesDto.java
@@ -1,4 +1,30 @@
 package com.autonomoussystemserver.server.controller.model;
 
+import com.autonomoussystemserver.server.database.model.Devices;
+
 public class InteractionTimesDto {
+
+    private Devices mascotId;
+    private Integer interactionTimes;
+
+
+    // for deserialisation
+    public InteractionTimesDto() {
+    }
+
+    public InteractionTimesDto(Devices mascotId, Integer interactionTimes) {
+        System.out.println("InteractionTimesDto constructor");
+        this.mascotId = mascotId;
+        this.interactionTimes = interactionTimes;
+    }
+
+    public Devices getMascotId() {
+        System.out.println("InteractionTimesDto getMascotId = " + mascotId);
+        return mascotId;
+    }
+
+    public Integer getInteractionTimes() {
+        System.out.println("InteractionTimesDto getInteractionTimes = " + interactionTimes);
+        return interactionTimes;
+    }
 }
