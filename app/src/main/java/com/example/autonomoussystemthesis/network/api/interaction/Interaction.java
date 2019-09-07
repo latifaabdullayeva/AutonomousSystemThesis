57b15946b59e1d353507c3e0f332c3a0d10213b8
Latifa <latifaabdullayeva@gmail.com>
2019-09-07 17:13:16 +0200

    Adds functionality for Interaction, ApiResponse and Service classes

--- app/src/main/java/com/example/autonomoussystemthesis/network/api/interaction/Interaction.java
+++ app/src/main/java/com/example/autonomoussystemthesis/network/api/interaction/Interaction.java
@@ -1,4 +1,25 @@
 package com.example.autonomoussystemthesis.network.api.interaction;
 
+// interactionTimes, mascot
+
+import android.util.Log;
+
 public class Interaction {
+    private final Integer interactionTimes;
+    private final Integer mascotId;
+
+    Interaction(Integer interactionTimes, Integer mascotId) {
+        Log.d("FLOW", "Interaction");
+
+        this.interactionTimes = interactionTimes;
+        this.mascotId = mascotId;
+    }
+
+    public Integer getInteractionTimes() {
+        return interactionTimes;
+    }
+
+    public Integer getMascotId() {
+        return mascotId;
+    }
 }
--- app/src/main/java/com/example/autonomoussystemthesis/network/api/interaction/InteractionService.java
+++ app/src/main/java/com/example/autonomoussystemthesis/network/api/interaction/InteractionService.java
@@ -1,4 +1,15 @@
 package com.example.autonomoussystemthesis.network.api.interaction;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public class InteractionService {
+    @POST("interactionTimes")
+    Call<ResponseBody> postInteraction(@Body Interaction interaction);
+
+    @GET("interactionTimes")
+    Call<ApiInteractionResponse> getInteractions();
 }
