74fa6ae5acf97aa50807fc5606542009591ca6df
Latifa <latifaabdullayeva@gmail.com>
2019-09-09 19:57:29 +0200

    Adds methods to for code optimization

--- app/src/main/java/com/example/autonomoussystemthesis/ShowAllDistances.java
+++ app/src/main/java/com/example/autonomoussystemthesis/ShowAllDistances.java
@@ -72,109 +72,121 @@ public class ShowAllDistances extends AppCompatActivity implements BeaconConsume
         devicePers.setText(devicePersValue);
     }
 
+    public void mascotVibration(ApiPersonalityResponse personalities, Beacon beacon, Device device, Vibrator vibrator, Integer myMascotId) {
+        if (personalities != null) {
+            for (Personality personality : personalities.getContent()) {
+                Log.d(TAG, "personality = " + personality);
+                Log.d(TAG, "personality.getPersonality_name() = " + personality.getPersonality_name());
+                Log.d(TAG, "device.getDevicePersonality().getPersonality_name() = " + device.getDevicePersonality().getPersonality_name());
+
+                // vibrate according approaching device's personality
+                if (personality.getPersonality_name().equals(device.getDevicePersonality().getPersonality_name())) {
+                    Log.d("test", "If personality DISTANCE == : " + round(beacon.getDistance() * 100));
+                    vibrator.vibrate(100 * personality.getVibration_level());
+                    Log.d(TAG, "personality.getVibration_level() = " + device.getDevicePersonality().getVibration_level());
+                    interactionRepository.sendNetworkRequest(myMascotId);
+                }
+            }
+        }
+    }
+
+    public void mascotMascotInteraction(Beacon beacon, Device device, Integer myMascotId) {
+
+        // When the type of our device is Mascot, We get all other devices from DB that are Mascots,
+        // then we check if the distance from my Mascot to any other Mascots is less or equal to 45 cm,
+        // we vibrate my mascot according to the personality of other mascot
+
+        // We check whether user's device and the approaching device both are Mascots and their distance is less or equal 45 cm
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        if (beacon.getDistance() <= 20 && deviceTypeValue.equals("Mascot") && device.getDeviceType().equals("Mascot")) {
+            Log.d("test", "If DISTANCE == : " + deviceTypeValue + "; " +
+                    device.getDeviceType() + "; " + round(beacon.getDistance() * 100));
+            // Add a vibration level according to the personality of a device to whom we measure the distance
+            personalityRepository.getNetworkRequest(new Callback<ApiPersonalityResponse>() {
+                @Override
+                public void onResponse(@NonNull Call<ApiPersonalityResponse> call, @NonNull Response<ApiPersonalityResponse> response) {
+                    if (!response.isSuccessful()) {
+                        Log.d(TAG, "PersonalityRepository Code: " + response.code());
+                        return;
+                    }
+                    ApiPersonalityResponse personalities = response.body();
+                    mascotVibration(personalities, beacon, device, vibrator, myMascotId);
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<ApiPersonalityResponse> call, @NonNull Throwable t) {
+                    Log.d(TAG, "error loading from API: " + t.getMessage());
+                }
+            });
+        } else if (beacon.getDistance() > 20) {
+            Log.d("test", "ELSE DISTANCE != : " + round(beacon.getDistance() * 100));
+            // if the distance is more than 45 cm, cancel vibration
+            vibrator.cancel();
+        }
+    }
+
+    public void getDeviceRequest(Collection<Beacon> beacons) {
+        deviceRepository.getNetworkRequest(new Callback<ApiDevicesResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ApiDevicesResponse> call, @NonNull Response<ApiDevicesResponse> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(TAG, "Code: " + response.code());
+                    return;
+                }
+                ApiDevicesResponse devices = response.body();
+
+                // find my own phone (device id)
+                Integer myDeviceID = null;
+                if (devices != null) {
+                    // we check if the beacon that the user has chosen exists in DB
+                    boolean myBeaconIsInDB = false;
+                    for (int i = 0; i < devices.getContent().size(); i++) {
+                        if (devices.getContent().get(i).getBeaconUuid().contains(beaconTagValue)) {
+                            myBeaconIsInDB = true;
+                            myDeviceID = devices.getContent().get(i).getDeviceId();
+                        }
+                    }
+                    // if the beacon exists in DB, continue, otherwise Log the message that "Your Beacon DOES NOT exists in DB"
+                    if (myBeaconIsInDB && beacons.size() > 0) {
+                        // if the numb of beacons in the room is not 0,
+                        // for every beacon check ..
+                        for (Beacon beacon : beacons) {
+                            // .. if beacon that user choose is not the same as beacon in the room, then we sendRequest with (from, to, distance)
+                            // if two endpoints are the same, then show the message that the distance between two identical endpoints are the same
+                            if (!beaconTagValue.equals(beacon.getId1().toString())) {
+                                for (Device device : devices.getContent()) {
+                                    if (device.getBeaconUuid().equals(beacon.getId1().toString())) {
+                                        distanceRepository.sendNetworkRequest(myDeviceID, device.getDeviceId(), round(beacon.getDistance() * 100));
+                                        mascotMascotInteraction(beacon, device, myDeviceID);
+                                    }
+                                }
+                            } else {
+                                // if two endpoints are the same, then show the message that the distance between two identical endpoints are the same
+                                Log.d(TAG, "= myDevice (" + beaconTagValue + "); device (" + beacon.getId1().toString() + ") = same");
+                            }
+
+                        }
+                    } else {
+                        Log.d(TAG, "Your Beacon DOES NOT exists in DB");
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ApiDevicesResponse> call,
+                                  @NonNull Throwable t) {
+                Log.d(TAG, "error loading from API... " + t.getMessage());
+            }
+        });
+    }
+
     @Override
     public void onBeaconServiceConnect() {
         beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                deviceRepository.getNetworkRequest(new Callback<ApiDevicesResponse>() {
-                    @Override
-                    public void onResponse(@NonNull Call<ApiDevicesResponse> call, @NonNull Response<ApiDevicesResponse> response) {
-                        if (!response.isSuccessful()) {
-                            Log.d(TAG, "Code: " + response.code());
-                            return;
-                        }
-                        ApiDevicesResponse devices = response.body();
-
-                        // find my own phone (device id)
-                        Integer myDeviceID = null;
-                        if (devices != null) {
-                            // we check if the beacon that the user has chosen exists in DB
-                            boolean myBeaconIsInDB = false;
-                            for (int i = 0; i < devices.getContent().size(); i++) {
-                                if (devices.getContent().get(i).getBeaconUuid().contains(beaconTagValue)) {
-                                    myBeaconIsInDB = true;
-                                    myDeviceID = devices.getContent().get(i).getDeviceId();
-                                }
-                            }
-                            // if the beacon exists in DB, continue, otherwise Log the message that "Your Beacon DOES NOT exists in DB"
-                            if (myBeaconIsInDB) {
-                                if (beacons.size() > 0) {
-                                    // if the numb of beacons in the room is not 0,
-                                    // for every beacon check ..
-                                    for (Beacon beacon : beacons) {
-                                        // .. if beacon that user choose is not the same as beacon in the room, then we sendRequest with (from, to, distance)
-                                        // if two endpoints are the same, then show the message that the distance between two identical endpoints are the same
-                                        if (!beaconTagValue.equals(beacon.getId1().toString())) {
-                                            for (Device device : devices.getContent()) {
-                                                if (device.getBeaconUuid().equals(beacon.getId1().toString())) {
-                                                    distanceRepository.sendNetworkRequest(myDeviceID, device.getDeviceId(), round(beacon.getDistance() * 100));
-                                                    Integer myMascotId = myDeviceID;
-
-                                                    // When the type of our device is Mascot, We get all other devices from DB that are Mascots,
-                                                    // then we check if the distance from my Mascot to any other Mascots is less or equal to 45 cm,
-                                                    // we vibrate my mascot according to the personality of other mascot
-
-                                                    // We check whether user's device and the approaching device both are Mascots and their distance is less or equal 45 cm
-                                                    final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-                                                    if (beacon.getDistance() <= 20 && deviceTypeValue.equals("Mascot") && device.getDeviceType().equals("Mascot")) {
-                                                        // Add a vibration level according to the personality of a device to whom we measure the distance
-                                                        personalityRepository.getNetworkRequest(new Callback<ApiPersonalityResponse>() {
-                                                            @Override
-                                                            public void onResponse(@NonNull Call<ApiPersonalityResponse> call, @NonNull Response<ApiPersonalityResponse> response) {
-                                                                if (!response.isSuccessful()) {
-                                                                    Log.d(TAG, "PersonalityRepository Code: " + response.code());
-                                                                    return;
-                                                                }
-                                                                ApiPersonalityResponse personalities = response.body();
-
-                                                                if (personalities != null) {
-                                                                    for (Personality personality : personalities.getContent()) {
-                                                                        Log.d(TAG, "personality = " + personality);
-                                                                        Log.d(TAG, "personality.getPersonality_name() = " + personality.getPersonality_name());
-                                                                        Log.d(TAG, "device.getDevicePersonality().getPersonality_name() = " + device.getDevicePersonality().getPersonality_name());
-
-                                                                        // vibrate according approaching device's personality
-                                                                        if (personality.getPersonality_name().equals(device.getDevicePersonality().getPersonality_name())) {
-                                                                            vibrator.vibrate(100 * personality.getVibration_level());
-                                                                            Log.d(TAG, "personality.getVibration_level() = " + device.getDevicePersonality().getVibration_level());
-                                                                            interactionRepository.sendNetworkRequest(myMascotId);
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-
-                                                            @Override
-                                                            public void onFailure(@NonNull Call<ApiPersonalityResponse> call, @NonNull Throwable t) {
-                                                                Log.d(TAG, "error loading from API: " + t.getMessage());
-                                                            }
-                                                        });
-                                                    } else if (beacon.getDistance() > 20) {
-                                                        // if the distance is more than 45 cm, cancel vibration
-                                                        vibrator.cancel();
-                                                    }
-                                                }
-                                            }
-                                        } else {
-                                            // if two endpoints are the same, then show the message that the distance between two identical endpoints are the same
-                                            Log.d(TAG, "= myDevice (" + beaconTagValue + "); device (" + beacon.getId1().toString() + ") = same");
-                                        }
-
-                                    }
-                                }
-                            } else {
-                                Log.d(TAG, "Your Beacon DOES NOT exists in DB");
-                            }
-
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(@NonNull Call<ApiDevicesResponse> call,
-                                          @NonNull Throwable t) {
-                        Log.d(TAG, "error loading from API... " + t.getMessage());
-                    }
-                });
+                getDeviceRequest(beacons);
             }
         });
         try {
