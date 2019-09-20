a8566826baf0e9aa9c6f3dd3d17484091ab606e6
Latifa <latifaabdullayeva@gmail.com>
2019-09-20 12:25:11 +0200

    Sets up Beacon Manager on Launch screen

--- MyTablet/app/src/main/java/com/example/mytablet/LaunchScreen.java
+++ MyTablet/app/src/main/java/com/example/mytablet/LaunchScreen.java
@@ -2,6 +2,24 @@ package com.example.mytablet;
 
 import android.app.Application;
 
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+
 public class LaunchScreen extends Application {
 
+    public void onCreate() {
+        super.onCreate();
+        setUpBeaconManager();
+    }
+
+    private void setUpBeaconManager() {
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().clear();
+
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
+        );
+        new BackgroundPowerSaver(this);
+    }
 }
