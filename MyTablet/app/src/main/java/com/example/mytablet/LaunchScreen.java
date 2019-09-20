6c400ec7da57007d67c3fc584c0460465486c087
Latifa <latifaabdullayeva@gmail.com>
2019-09-20 12:15:44 +0200

    Adds LaunchScreen, TabletInitialisation, get app permissions

---
+++ MyTablet/app/src/main/java/com/example/mytablet/LaunchScreen.java
@@ -0,0 +1,7 @@
+package com.example.mytablet;
+
+import android.app.Application;
+
+public class LaunchScreen extends Application {
+
+}
---
+++ MyTablet/app/src/main/java/com/example/mytablet/TabletInitialisation.java
@@ -0,0 +1,6 @@
+package com.example.mytablet;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class TabletInitialisation extends AppCompatActivity {
+}
